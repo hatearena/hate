667d753f6e5b4635ec04fd227541c712689990c0
Navid M <navidmomtahen@gmail.com>
2026-05-29 12:47:44 +0100

    render: update sigil

--- src/renderextras.cpp
+++ src/renderextras.cpp
@@ -303,7 +303,7 @@ void gl_drawhud(int w, int h, int curfps, int nquads, int curvert,
   char *command = getcurcommand();
   char *player = playerincrosshair();
   if (command)
-    draw_textf("> %s_", 20, 1400, 2, command);
+    draw_textf("$ %s_", 20, 1400, 2, command);
   else if (player)
     draw_text(player, 20, 1400, 2);
 
