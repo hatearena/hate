7052df578bab712f26ee952e09ae5112170720fd
Navid M <navidmomtahen@gmail.com>
2026-05-30 17:49:11 +0100

    clientextras: amend death indicator on scoreboard

--- src/clientextras.cpp
+++ src/clientextras.cpp
@@ -91,7 +91,7 @@ vector<sline> scorelines;
 
 void renderscore(dynent *d) {
   sprintf_sd(lag)("%d", d->plag);
-  sprintf_sd(name)("(%s)", d->name);
+  sprintf_sd(name)("%s [Dead]", d->name);
   sprintf_s(scorelines.add().s)("%d\t%s\t%d\t%s\t%s", d->frags,
                                 d->state == CS_LAGGED ? "LAG" : lag, d->ping,
                                 d->team, d->state == CS_DEAD ? name : d->name);
