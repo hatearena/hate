4c51fea87ad2577fd0401b5fbc5019e0e3f11c78
Navid M <navidmomtahen@gmail.com>
2026-05-31 11:51:26 +0100

    main: capitalize

--- src/main.cpp
+++ src/main.cpp
@@ -210,7 +210,7 @@ int main(int argc, char **argv) {
       !installtex(4, path(newstring("data/explosion.jpg")), xs, ys) ||
       !installtex(5, path(newstring("data/items.png")), xs, ys) ||
       !installtex(1, path(newstring("data/crosshair.png")), xs, ys))
-    fatal("could not find core textures (hint: run HATE from the parent of the "
+    fatal("Could not find core textures (hint: run HATE from the parent of the "
           "bin directory)");
 
   log("font");
