2a81b964c29bfe801cca758dbf42b68c40e80b9e
Navid M <navidmomtahen@gmail.com>
2026-05-31 18:32:32 +0100

    editing: fix graphical issue

--- src/renderextras.cpp
+++ src/renderextras.cpp
@@ -1,13 +1,11 @@
 #include "cube.h"
 
 void line(int x1, int y1, float z1, int x2, int y2, float z2) {
-  glBegin(GL_POLYGON);
+  glBegin(GL_LINE_STRIP);
   glVertex3f((float)x1, z1, (float)y1);
-  glVertex3f((float)x1, z1, y1 + 0.01f);
-  glVertex3f((float)x2, z2, y2 + 0.01f);
   glVertex3f((float)x2, z2, (float)y2);
   glEnd();
-  xtraverts += 4;
+  xtraverts += 2;
 };
 
 void linestyle(float width, int r, int g, int b) {
--- src/rendergl.cpp
+++ src/rendergl.cpp
@@ -922,8 +922,13 @@ void gl_drawframe(int w, int h, float curfps) {
   glDisable(GL_TEXTURE_2D);
 
   if (editmode) {
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     extern void cursorupdate();
     cursorupdate();
+    glEnable(GL_DEPTH_TEST);
+    glDisable(GL_BLEND);
   };
 
   gl_drawhud(w, h, (int)curfps, nquads, curvert, underwater);
