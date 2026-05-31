ee493963174b7dad25c6e772d9574cb1b40dbdc4
Navid M <navidmomtahen@gmail.com>
2026-05-31 02:46:26 +0100

    rendertext: glyph rendering fixes

--- src/rendertext.cpp
+++ src/rendertext.cpp
@@ -5,101 +5,74 @@
 static TTF_Font *font = NULL;
 static bool font_inited = false;
 
-struct glyph
-{
-    GLuint texid;
-    short w, h;
-    short advance;
-    short minx, miny;
-};
-static glyph glyphs[95];
-
 void init_font()
 {
     font = TTF_OpenFont(path(newstring("data/font.ttf")), FONTH);
     if (!font) fatal("could not load font data/font.ttf");
+    font_inited = true;
+}
 
-    for (int i = 0; i < 95; i++)
+static void render_surface(SDL_Surface *s, int x, int y)
+{
+    if (!s) return;
+    int w = s->w, h = s->h;
+    int spitch = s->pitch / 4;
+
+    SDL_Surface *rgb = SDL_CreateRGBSurface(0, w, h, 24, 0x0000FF, 0x00FF00, 0xFF0000, 0);
+    if (!rgb) { SDL_FreeSurface(s); return; }
+
+    SDL_LockSurface(s);
+    SDL_LockSurface(rgb);
+
+    for (int gy = 0; gy < h; gy++)
     {
-        Uint16 ch = (Uint16)(unsigned char)(i + 33);
-        int minx, maxx, miny, maxy, advance;
-        TTF_GlyphMetrics(font, ch, &minx, &maxx, &miny, &maxy, &advance);
-        glyphs[i].advance = advance;
-        glyphs[i].minx = minx;
-        glyphs[i].miny = miny;
-
-        SDL_Color white = {255, 255, 255, 255};
-        SDL_Surface *s = TTF_RenderGlyph_Blended(font, ch, white);
-        if (!s)
+        for (int gx = 0; gx < w; gx++)
         {
-            glyphs[i].texid = 0;
-            glyphs[i].w = 0;
-            glyphs[i].h = 0;
-            continue;
+            Uint32 pixel = ((Uint32 *)s->pixels)[gy * spitch + gx];
+            Uint8 r, g, b, a;
+            SDL_GetRGBA(pixel, s->format, &r, &g, &b, &a);
+            Uint8 *dst = (Uint8 *)rgb->pixels + gy * rgb->pitch + gx * 3;
+            dst[0] = a;
+            dst[1] = a;
+            dst[2] = a;
         }
-
-        int sw = s->w, sh = s->h;
-        int src_pitch_pix = s->pitch / 4;
-
-        SDL_Surface *rgb = SDL_CreateRGBSurface(0, sw, sh, 24, 0x0000FF, 0x00FF00, 0xFF0000, 0);
-        if (!rgb) fatal("could not create glyph surface");
-
-        SDL_LockSurface(s);
-        SDL_LockSurface(rgb);
-
-        for (int gy = 0; gy < sh; gy++)
-        {
-            for (int gx = 0; gx < sw; gx++)
-            {
-                Uint32 pixel = ((Uint32 *)s->pixels)[gy * src_pitch_pix + gx];
-                Uint8 r, g, b, a;
-                SDL_GetRGBA(pixel, s->format, &r, &g, &b, &a);
-                Uint8 val = a;
-                Uint8 *dst = (Uint8 *)rgb->pixels + gy * rgb->pitch + gx * 3;
-                dst[0] = val;
-                dst[1] = val;
-                dst[2] = val;
-            }
-        }
-
-        SDL_UnlockSurface(rgb);
-        SDL_UnlockSurface(s);
-        SDL_FreeSurface(s);
-
-        GLuint tex;
-        glGenTextures(1, &tex);
-        glBindTexture(GL_TEXTURE_2D, tex);
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-        gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, sw, sh, GL_RGB, GL_UNSIGNED_BYTE, rgb->pixels);
-        SDL_FreeSurface(rgb);
-
-        glyphs[i].texid = tex;
-        glyphs[i].w = sw;
-        glyphs[i].h = sh;
     }
 
-    font_inited = true;
+    SDL_UnlockSurface(rgb);
+    SDL_UnlockSurface(s);
+
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, w, h, GL_RGB, GL_UNSIGNED_BYTE, rgb->pixels);
+    SDL_FreeSurface(rgb);
+
+    glBlendFunc(GL_ONE, GL_ONE);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0, 0); glVertex2i(x, y);
+    glTexCoord2f(1, 0); glVertex2i(x + w, y);
+    glTexCoord2f(1, 1); glVertex2i(x + w, y + h);
+    glTexCoord2f(0, 1); glVertex2i(x, y + h);
+    glEnd();
+    xtraverts += 4;
+
+    glDeleteTextures(1, &tex);
+    SDL_FreeSurface(s);
 }
 
 int text_width(char *str)
 {
-    int x = 0;
-    for (int i = 0; str[i] != 0; i++)
-    {
-        int c = (unsigned char)str[i];
-        if (c == '\t') { x = (x + PIXELTAB) / PIXELTAB * PIXELTAB; continue; }
-        if (c == '\f') continue;
-        if (c == ' ') { x += FONTH / 2; continue; }
-        c -= 33;
-        if (c < 0 || c >= 95 || !font_inited) continue;
-        x += glyphs[c].advance;
-    }
-    return x;
+    if (!font_inited) return 0;
+    int w;
+    TTF_SizeUTF8(font, str, &w, NULL);
+    return w;
 }
 
 void draw_textf(char *fstr, int left, int top, int gl_num, ...)
@@ -112,38 +85,17 @@ void draw_text(char *str, int left, int top, int gl_num)
 {
     (void)gl_num;
     if (!font_inited) return;
-    glBlendFunc(GL_ONE, GL_ONE);
-    glColor3ub(255, 255, 255);
 
-    int x = left;
-    int y = top;
-
-    for (int i = 0; str[i] != 0; i++)
+    SDL_Color white = {255, 255, 255, 255};
+    if (str[0] == '\f')
     {
-        int c = (unsigned char)str[i];
-        if (c == '\t') { x = (x - left + PIXELTAB) / PIXELTAB * PIXELTAB + left; continue; }
-        if (c == '\f') { glColor3ub(64, 255, 128); continue; }
-        if (c == ' ') { x += FONTH / 2; continue; }
-        c -= 33;
-        if (c < 0 || c >= 95) continue;
-        if (!glyphs[c].texid) continue;
-
-        glBindTexture(GL_TEXTURE_2D, glyphs[c].texid);
-
-        int dx = x + glyphs[c].minx;
-        int dy = y;
-        int dw = glyphs[c].w;
-        int dh = glyphs[c].h;
-
-        glBegin(GL_QUADS);
-        glTexCoord2f(0, 0); glVertex2i(dx, dy);
-        glTexCoord2f(1, 0); glVertex2i(dx + dw, dy);
-        glTexCoord2f(1, 1); glVertex2i(dx + dw, dy + dh);
-        glTexCoord2f(0, 1); glVertex2i(dx, dy + dh);
-        glEnd();
-
-        xtraverts += 4;
-        x += glyphs[c].advance;
+        glColor3ub(64, 255, 128);
+        render_surface(TTF_RenderUTF8_Blended(font, str + 1, white), left, top);
+    }
+    else
+    {
+        glColor3ub(255, 255, 255);
+        render_surface(TTF_RenderUTF8_Blended(font, str, white), left, top);
     }
 }
 
