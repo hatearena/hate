c0e3a8cb41547ea739a6ac5cde6c0c78036dd7ba
Navid M <navidmomtahen@gmail.com>
2026-06-01 11:31:17 +0100

    weapon: users must be onfloor for shotgun-jump to work

--- src/weapon.cpp
+++ src/weapon.cpp
@@ -394,7 +394,7 @@ void shoot(dynent *d, vec &targ) {
   vec kickback = unitv;
   vmul(kickback, guns[d->gunselect].kickamount * -0.01f);
   vadd(d->vel, kickback);
-  if (d->gunselect == GUN_SG && d->pitch < -20.0f)
+  if (d->gunselect == GUN_SG && d->pitch < -20.0f && d->onfloor)
     d->vel.z += -d->pitch / 90.0f * 3.0f;
   if (d->pitch < 80.0f)
     d->pitch += guns[d->gunselect].kickamount * 0.05f;
