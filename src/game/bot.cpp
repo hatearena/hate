ec843093b1d8ee221fe0f1fa2e31c4db78da0662
Navid M <navidmomtahen@gmail.com>
2026-06-06 00:10:55 +0100

    bot: add further names

--- src/game/bot.cpp
+++ src/game/bot.cpp
@@ -12,10 +12,11 @@ VAR(botamount, 1, 4, 16);
 static int botspawncycle = -1;
 
 static const char *botnames[] = {
-    "Cerelo", "Diaso", "Ceria",   "Deathly", "Ra",    "Va",    "Never", "Abu",
-    "Re",     "Why",   "Lucky",   "Lano",    "Cliff", "Cobra", "Liner", "Chiba",
-    "Dragon", "Sabre", "Koffman", "Stuff",   "Bones", "Xor",   "Snuff", "Sniff",
-    "Pain",   "Time",  "Fake",    "Headup",  "MX",    "Moon",  "Wine",  "Tux"};
+    "Cerelo", "Diaso", "Ceria",  "Deathly", "Ra",      "Va",     "Never",
+    "Abu",    "Re",    "Why",    "Lucky",   "Lano",    "Cliff",  "Cobra",
+    "Liner",  "Chiba", "Dragon", "Sabre",   "Koffman", "Stuff",  "Bones",
+    "Xor",    "Snuff", "Sniff",  "Pain",    "Time",    "Fake",   "Headup",
+    "MX",     "Moon",  "Wine",   "Tux",     "Crash",   "Threed", "Backlotter"};
 
 static const int numbotnames = sizeof(botnames) / sizeof(botnames[0]);
 
