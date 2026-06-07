074d1badae81caf6ff409d35b1ede8dd82ade855
Navid M <navidmomtahen@gmail.com>
2026-06-07 18:00:19 +0100

    remodel-pickups: nailgun alignment fixes

--- src/game/entities.cpp
+++ src/game/entities.cpp
@@ -7,7 +7,7 @@ char *entmdlnames[] = {
     "boost",  "g_armour", "y_armour", "quad",    "teleporter",
 };
 float entzoffsets[] = {
-    -0.4f, -0.3f, 0.2f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+    -0.4f, -0.3f, -0.05f, 1.0f, -0.25f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
 };
 
 int triggertime = 0;
