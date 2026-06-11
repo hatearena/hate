7b642ff489c9ee5b98fa1ab45110fd2324585544
Navid M <navidmomtahen@gmail.com>
2026-06-11 18:49:01 +0100

    cmake: mac app bundle generation

--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
-cd src
-make
-cd ..
+#!/bin/bash
+set -e
 
+UNAME_S=$(uname -s)
+
+mkdir -p build
+cd build
+
+if [ "$UNAME_S" = "Darwin" ]; then
+    echo "Building for MacOS..."
+    cmake .. -DCMAKE_BUILD_TYPE=Release
+    make -j$(sysctl -n hw.ncpu)
+    echo "Build complete."
+else
+    echo "Building for Linux with AppImage..."
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_APPIMAGE=ON
+    make -j$(nproc)
+    make appimage
+    echo "Build complete. AppImage is in build/"
+fi
