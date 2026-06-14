993ec85d583485698a82b0970b49912e524a2047
Navid M <navidmomtahen@gmail.com>
2026-06-14 13:49:33 +0100

    rgl: amend

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -1423,13 +1423,9 @@ void drawhudgun(float fovy, float aspect, int farplane) {
   int rtime = reloadtime(d->gunselect);
   if (d->lastaction && d->lastattackgun == d->gunselect &&
       lastmillis - d->lastaction < rtime) {
-    if (d->gunselect == GUN_RIFLE) {
-      int sgtime = lastmillis - d->lastaction;
-      if (sgtime < 1050)
-        drawhudmodel(5, 5, 150.0f, d->lastaction);
-      else
-        drawhudmodel(6, 10, 200, 0);
-    } else if (d->gunselect == GUN_SG) {
+    if (d->gunselect == GUN_RIFLE)
+      drawhudmodel(6, 4, 400, d->lastaction);
+    else if (d->gunselect == GUN_SG) {
       int sgtime = lastmillis - d->lastaction;
       if (sgtime < 840)
         drawhudmodel(11, 11, 100.0f, d->lastaction);
